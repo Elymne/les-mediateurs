ac62788dc125e2dc79a1719e95d89ce816e65bec
Djurdjevic Sacha <31738821+Elymne@users.noreply.github.com>
2023-11-11 03:29:03 +0100

    (quash) creating background

--- lib/features/home/presentation/widgets/home_background.dart
+++ lib/features/home/presentation/widgets/home_background.dart
@@ -11,6 +11,31 @@ class HomeBackground extends ConsumerStatefulWidget {
 class _State extends ConsumerState<HomeBackground> {
   @override
   Widget build(BuildContext context) {
-    return Stack();
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            'assets/images/background_1.jpg',
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        color: const Color(0x00000000).withOpacity(0.85),
+        child: Align(
+          //alignment: Alignment(x, size.height / 2),
+          child: Text(
+            "PETIT TEST Là",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 100,
+              fontWeight: FontWeight.w100,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
