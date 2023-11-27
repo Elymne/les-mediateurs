bc5b9f3c133073ef94a42f8ce50b025846fe15b5
Elymne <Elymne@users.noreply.github.com>
2023-11-27 09:23:21 +0000

    deploy: cb10a3a5278364ad38b5d505a9dcd7c2a98df7eb

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,8 +7,8 @@ const RESOURCES = {"manifest.json": "a86e0ad4db6651e65b43c05ea3bc8482",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "version.json": "1fc7ead3d66df19f35d58ef4b03bea5b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "ed1333e991211ecd0751f0fa52afdaab",
-"/": "ed1333e991211ecd0751f0fa52afdaab",
+"index.html": "e209e22d6ae77176022d0b360521294e",
+"/": "e209e22d6ae77176022d0b360521294e",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -56,7 +56,7 @@ const RESOURCES = {"manifest.json": "a86e0ad4db6651e65b43c05ea3bc8482",
 "canvaskit/canvaskit.wasm": "64edb91684bdb3b879812ba2e48dd487",
 "canvaskit/chromium/canvaskit.js": "0ae8bbcc58155679458a0f7a00f66873",
 "canvaskit/chromium/canvaskit.wasm": "f87e541501c96012c252942b6b75d1ea",
-"main.dart.js": "7364c26dff5e1cad4bf95db5c0113a51"};
+"main.dart.js": "d0d425612dbd9e262e0758a3198d86b6"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
